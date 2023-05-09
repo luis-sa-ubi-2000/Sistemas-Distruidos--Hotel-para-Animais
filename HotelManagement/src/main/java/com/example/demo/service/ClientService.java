0739cdab8246618b146326d78b3b472824899bf5
João Baltazar <joaobaltazar55@gmail.com>
2023-05-09 22:06:55 +0100

    client services

--- HotelManagement/src/main/java/com/example/demo/service/ClientService.java
+++ HotelManagement/src/main/java/com/example/demo/service/ClientService.java
@@ -1,5 +1,12 @@
 package com.example.demo.service;
 
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+
+import com.example.demo.model.Client;
+
 public interface ClientService {
+	List<Client> getAllClients();
 
 }
--- HotelManagement/src/main/java/com/example/demo/service/ClientServiceImpl.java
+++ HotelManagement/src/main/java/com/example/demo/service/ClientServiceImpl.java
@@ -1,5 +1,20 @@
 package com.example.demo.service;
 
-public class ClientServiceImpl {
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.example.demo.model.Client;
+import com.example.demo.repository.ClientRepository;
+
+public class ClientServiceImpl implements ClientService {
+	
+	@Autowired
+	private ClientRepository  clientRepository;
+
+	@Override
+	public List<Client> getAllClients() {
+		return clientRepository.findAll();
+	}
 
 }
