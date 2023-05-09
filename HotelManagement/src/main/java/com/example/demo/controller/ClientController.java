e818380acc5b559a57808534185c87a586866637
João Baltazar <joaobaltazar55@gmail.com>
2023-05-09 22:50:02 +0100

    Index adequado, problema não aparece info

--- HotelManagement/src/main/java/com/example/demo/controller/ClientController.java
+++ HotelManagement/src/main/java/com/example/demo/controller/ClientController.java
@@ -1,5 +1,22 @@
 package com.example.demo.controller;
 
-public class ClientController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
+import com.example.demo.service.ClientService;
+
+
+public class ClientController {
+	@Autowired
+	private ClientService clientService;
+	
+	
+	
+	// display list of employees
+	@GetMapping("/")
+	public String viewHomePage(Model model) {
+		model.addAttribute("listClients", clientService.getAllClients());
+		return "index";
+	}
 }
