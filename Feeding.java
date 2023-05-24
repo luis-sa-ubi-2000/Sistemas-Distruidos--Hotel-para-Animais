a6c4078ad3e723f8e415b170b9a8449750bb7497
luis-sa-ubi-2000 <118893248+luis-sa-ubi-2000@users.noreply.github.com>
2023-05-24 19:59:15 +0100

    Add files via upload

---
+++ Feeding.java
@@ -0,0 +1,68 @@
+package com.example.demo.model;
+
+
+
+import jakarta.persistence.*;
+
+@Entity
+public class Feeding {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "id_pet", referencedColumnName = "id")
+    private Pet pet;
+
+    @Column(name = "type_food")
+    private String typeFood;
+
+
+    // Constructors, getters, and setters
+
+    public Feeding() {
+    }
+
+    public Feeding(Pet pet, Room room, String typeFood) {
+        this.pet = pet;
+        this.typeFood = typeFood;
+    }
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Pet getPet() {
+		return pet;
+	}
+
+	public void setPet(Pet pet) {
+		this.pet = pet;
+	}
+
+	public String gettypeFood() {
+		return typeFood;
+	}
+
+	public void settypeFood(String typeFood) {
+		this.typeFood = typeFood;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Lodging [id=" + id + ", pet=" + pet + ", tipoAlimentacao=" + typeFood + "]";
+	}
+
+    
+	
+
+
+    
+    
+}
+
