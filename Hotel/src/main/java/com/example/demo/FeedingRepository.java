fbe4831ab605a18b7e861df8af6d530261b75eef
luis-sa-ubi-2000 <luis.afonso.sa@ubi.pt>
2023-06-01 00:22:46 +0100

    Update FeedingRepository.java

--- Hotel/src/main/java/com/example/demo/FeedingRepository.java
+++ Hotel/src/main/java/com/example/demo/FeedingRepository.java
@@ -8,6 +8,7 @@ import org.springframework.data.repository.CrudRepository;
 public interface FeedingRepository extends CrudRepository<Feeding, Long>{
  	
 	
+<<<<<<< Updated upstream
 	@Query("SELECT f.typeFood, COUNT(p.id) AS petCount " +
             "FROM Feeding f " +
             "INNER JOIN f.pet p " +
@@ -16,5 +17,28 @@ public interface FeedingRepository extends CrudRepository<Feeding, Long>{
 
 
 
+=======
+	@Query("SELECT p.specie, f.typeFood " +
+	           "FROM Feeding f " +
+	           "INNER JOIN f.pet p " +
+	           "GROUP BY p.specie, f.typeFood " +
+	           "ORDER BY COUNT(*) DESC")
+    	List<Object[]> mostRequestedTypeOfFoodBySpecie();
+ 	
+ 	/* "SELECT p.specie, f.typeFood, COUNT(*) AS quantidade" +
+			"FROM Feeding f " +
+			"INNER JOIN f.pet p " +
+			"GROUP BY p.specie, f.typeFood " +
+			"HAVING quantidade = ( " +
+			"SELECT MAX(quantidade) " +
+			"FROM (" +
+			"SELECT COUNT(*) AS quantidade " +
+			"FROM Feeding ff " +
+			"JOIN ff.pet pp " +
+			"WHERE ff.typeFood = f.typeFood " +
+			"GROUP BY pp.specie, ff.typeFood " +
+			") AS subquery " +
+			") "*/
+>>>>>>> Stashed changes
 
 }
